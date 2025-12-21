9e71a92fc4d983ab9cc2a45ae7216a5e12cbd68e
Krishna Prajapat <krishnaprajapat693@gmail.com>
2025-12-21 17:28:02 +0530

    URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-export const backendURL = 'http://localhost:5000/api'
+export const backendURL = 'https://project-assessment-1.onrender.com/api'
+
+// export const backendURL = 'http://localhost:5000/api'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
